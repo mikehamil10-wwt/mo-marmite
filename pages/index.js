0604de77e4ca6c3d7d865143554f54710d90a53e
hamiltom <mike.hamilton@wwt.com>
2022-07-05 20:03:26 -0500

    feat: Contentful Client; recipe query

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
-export default function Recipes() {
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const contentful = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const res = await contentful.getEntries({
+    content_type: "recipe",
+  });
+
+  return {
+    props: {
+      recipes: res.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+
   return (
     <div className="recipe-list">
-      Recipe List
+      {recipes.map((recipe) => (
+        <div key={recipe.sys.id}>{recipe.fields.title}</div>
+      ))}
     </div>
-  )
+  );
 }
